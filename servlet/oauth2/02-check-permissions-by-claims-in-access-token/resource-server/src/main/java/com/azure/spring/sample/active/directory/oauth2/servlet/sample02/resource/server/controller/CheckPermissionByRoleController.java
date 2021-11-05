f0bbd0c48020855ae5d9dbd788e110fa9ff7dac0
Rujun Chen <rujche@microsoft.com>
2021-11-05 17:08:37 +0800

    Fix error in sample 02.

--- servlet/oauth2/02-check-permissions-by-claims-in-access-token/resource-server/src/main/java/com/azure/spring/sample/active/directory/oauth2/servlet/sample02/resource/server/controller/CheckPermissionByRoleController.java
+++ servlet/oauth2/02-check-permissions-by-claims-in-access-token/resource-server/src/main/java/com/azure/spring/sample/active/directory/oauth2/servlet/sample02/resource/server/controller/CheckPermissionByRoleController.java
@@ -9,13 +9,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class CheckPermissionByRoleController {
 
     @GetMapping("/role/resource-server-1-role-1")
-    @PreAuthorize("hasAuthority('ROLE_resource-server-1.role-1')")
+    @PreAuthorize("hasAuthority('ROLE_resource-server-1-role-1')")
     public String resourceServer1Role1() {
         return "Hi, this is resource-server-1. You can access my endpoint: /role/resource-server-1-role-1";
     }
 
     @GetMapping("/role/resource-server-1-role-2")
-    @PreAuthorize("hasAuthority('ROLE_resource-server-1.role-2')")
+    @PreAuthorize("hasAuthority('ROLE_resource-server-1-role-2')")
     public String resourceServer1Role2() {
         return "Hi, this is resource-server-1. You can access my endpoint: /role/resource-server-1-role-2";
     }
